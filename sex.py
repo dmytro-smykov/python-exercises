8da4e788af89d1e2a55f205ab84c3b5f3ea2ade9
Dmytro Smykov <dmytrosmykov@gmail.com>
2023-04-06 19:51:45 +0300

    Задание пол

---
+++ sex.py
@@ -0,0 +1,11 @@
+your_sex = input('Введите Ваш пол (м/ж): ')
+your_partner_sex = input('Введите пол того, кто Вам нравится: ')
+
+if your_sex == 'м' and your_partner_sex == 'ж':
+    print('Ты нормальный мужик!')
+if your_sex == 'м' and your_partner_sex == 'м':
+    print('Ты пидор!')
+if your_sex == 'ж' and your_partner_sex == 'м':
+    print('Ты нормальная баба!')
+if your_sex == 'ж' and your_partner_sex == 'ж':
+    print('Ты лесба!')
